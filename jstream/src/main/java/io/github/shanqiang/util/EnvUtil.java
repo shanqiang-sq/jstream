67986ebf69e542560ce4eb44bfccf228e7e6ec0f
shanqiang-sq <88421285+shanqiang-sq@users.noreply.github.com>
2022-06-10 23:15:57 +0800

    fix NPE

--- jstream/src/main/java/io/github/shanqiang/util/EnvUtil.java
+++ jstream/src/main/java/io/github/shanqiang/util/EnvUtil.java
@@ -1,22 +1,21 @@
 package io.github.shanqiang.util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class EnvUtil {
     private static Properties properties = null;
+
     public static synchronized String getEnvProperty(String key) {
         if (null == properties) {
             String env = System.getProperty("env");
             if (null == env) {
                 throw new IllegalArgumentException("unspecified -Denv param, use like \"java -Denv=prd ...\" to run");
             }
-            String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-            String configPath = rootPath + env + ".properties";
+
             properties = new Properties();
             try {
-                properties.load(new FileInputStream(configPath));
+                properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(env + ".properties"));
             } catch (IOException e) {
                 throw new IllegalStateException(e);
             }
